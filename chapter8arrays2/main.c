ad688242a1d180a75509bbcc0f414053aa05307d
Nicholas Morris <PrinceMorris@nicholas-morriss-imac.local>
2018-12-13 19:17:32 +1000

    Initial Commit

---
+++ chapter8arrays2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  chapter8arrays2
+//
+//  Created by Nicholas Morris on 13/12/18.
+//  Copyright © 2018 Nicholas Morris. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
